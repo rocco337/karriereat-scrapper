e3f3f37204962273cd4a356a9397b010b08006ca
roko bobic <=>
2019-03-22 21:05:58 +0100

    add pages

--- main.go
+++ main.go
@@ -1,83 +1,92 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net"
 	"net/http"
-	"time"
-	"github.com/gocolly/colly"
-	"encoding/json"
 	"os"
+	"time"
+
+	"github.com/gocolly/colly"
 )
 
 const firstJobPageUrl string = "https://www.karriere.at/jobs?page=%d"
 
 func main() {
-    fmt.Println("Starting...")
+	fmt.Println("Starting...")
 
 	var jobDetailsChannel chan *JobsDetails = make(chan *JobsDetails)
 	go saveResultToFile(jobDetailsChannel)
 
 	start := time.Now()
-	collector:= getNewColletcor()
-	scrapPage(fmt.Sprintf(firstJobPageUrl, 1),collector, jobDetailsChannel)
-	//scrapPage(fmt.Sprintf(firstJobPageUrl, 2), collector, jobDetailsChannel)
-	
+
+	noOfPAges := 10
+	scrapPage(noOfPAges, jobDetailsChannel)
+	//wg.Wait()
 	elapsed := time.Since(start)
-    fmt.Println("Elapsed", elapsed)
+	fmt.Println("Elapsed", elapsed)
 }
 
-func scrapPage(pageUrl string, collector *colly.Collector, c chan *JobsDetails) {	
-	collector.OnHTML(".m-jobItem__titleLink", func(e *colly.HTMLElement) {
-		link := e.Attr("href")
+func scrapPage(noOfPAges int, c chan *JobsDetails) {
+	collector := getNewColletcor()
 
-		collector.Visit(link)
+	collector.OnHTML(".m-jobItem__titleLink", func(e *colly.HTMLElement) {
+		go collector.Visit(e.Attr("href"))
 	})
 
 	collector.OnHTML(".c-jobDetail", func(e *colly.HTMLElement) {
-		result := new(JobsDetails)			
+		result := new(JobsDetails)
+		result.Url = e.Request.URL.String()
 		result.Title = e.DOM.Find(".m-jobHeader__container .m-jobHeader__jobTitle").Text()
 		result.Company = e.DOM.Find(".m-jobHeader__container .m-jobHeader__companyName").Text()
-		
-		metaItems:= e.DOM.Find(".m-jobHeader__container .m-jobHeader__metaList li");
-		result.Location=metaItems.First().Text()
-		result.Date= metaItems.Last().Text()
-		
-		html:= e.DOM.Find(".m-jobContent__jobText").Text()
-		result.Content =html;
-		c <- result		
+
+		metaItems := e.DOM.Find(".m-jobHeader__container .m-jobHeader__metaList li")
+		result.Location = metaItems.First().Text()
+		result.Date = metaItems.Last().Text()
+
+		html := e.DOM.Find(".m-jobContent__jobText").Text()
+		result.Content = html
+		//c <- result
 	})
 
 	collector.OnRequest(func(r *colly.Request) {
-		fmt.Println("\nVisiting", r.URL.String())
+		//fmt.Println("Visiting", r.URL.String())
 	})
 
 	collector.OnError(func(r *colly.Response, err error) {
 		fmt.Println("Request URL:", r.Request.URL, "failed with response:", r, "\nError:", err)
 	})
 
-	collector.Visit(pageUrl)
-	collector.Wait()	
+	i := 1
+	for i <= noOfPAges {
+		url := fmt.Sprintf(firstJobPageUrl, i)
+		fmt.Println("Scrapping", url)
+		collector.Visit(url)
+		i = i + 1
+	}
+
+	collector.Wait()
 }
 
-func saveResultToFile(jobDetailsChannel chan *JobsDetails){
+func saveResultToFile(jobDetailsChannel chan *JobsDetails) {
 	os.Remove("result.json")
-	f, err := os.OpenFile("result.json",os.O_CREATE|os.O_WRONLY, os.ModeAppend) 
+	f, err := os.OpenFile("result.json", os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		panic(err)
 	}
 
-	sep:="\n"
+	sep := "\n"
 	defer f.Close()
 	for {
-		result := <- jobDetailsChannel		
+		result := <-jobDetailsChannel
 		json, err := json.Marshal(result)
 		if err != nil {
-			panic(err)				
+			panic(err)
 		}
 		//fmt.Println("", result.url, result.company, result.title, result.location, result.date)
 
-		f.WriteString(string(json) + sep) 	
+		f.WriteString(string(json) + sep)
 	}
 }
 
@@ -111,4 +120,3 @@ type JobsDetails struct {
 	Date     string
 	Content  string
 }
-
